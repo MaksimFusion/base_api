7c15fdfd41d9d9135b86688dcc27444ee951a91d
MaksimFusion <maksim.foundation@gmail.com>
2022-02-01 13:16:42 +0300

    first commit

---
+++ models/model.js
@@ -0,0 +1,12 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    name: {type: DataTypes.STRING,  defaultValue: "Пользователь"},
+    dob: {type: DataTypes.DATE, defaultValue: DataTypes.NOW}
+})
+
+module.exports = User
